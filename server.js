5f300a273a6269eb519a2b7778ca1d93025a5269
sanjay-124 <mrsanjay2709@gmail.com>
2024-10-25 03:34:24 +0100

    n

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ io.on('connection', (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5173;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
